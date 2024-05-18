8af68d5838e8cc128d8be169b6050ce6085e7efb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-05-18 22:52:57 +0100

    feat: handle threads

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    let wbuf = b"+PONG\r\n";
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    break;
+                }
+                let _ = stream.write_all(wbuf);
+            }
+            Err(_) => {}
+        }
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-
-                let mut buf = [0; 512];
-                loop {
-                    let read_count = stream.read(&mut buf).unwrap();
-                    if read_count == 0 {
-                        break;
-                    }
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+            Ok(stream) => {
+                let _ = std::thread::spawn(move || handle_connection(stream));
             }
             Err(e) => {
                 println!("error: {}", e);
